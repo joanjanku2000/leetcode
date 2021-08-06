3d25ea27a4e18ae34d50a9ef24480a08dc63a39b
jjanku <janku.joan4@gmail.com>
2021-08-06 09:30:16 +0200

    median of 2 sorted arrays

--- src/leet/assessment/Order.java
+++ src/leet/assessment/Order.java
@@ -5,22 +5,21 @@ import java.util.List;
 
 public class Order {
 	/**
-	 * You are given two strings order and s. All the words of order are unique and were sorted in some custom order previously.
-
-Permute the characters of s so that they match the order that order was sorted. More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
-
+	 * You are given two strings order and s. 
+	 * All the words of order are unique and were sorted in some custom order previously.
+	 * Permute the characters of s so that they match the order that order was sorted.
+	 * More specifically, if a character x occurs before a character y in order, then x should occur
+	 *  before y in the permuted string.
 Return any permutation of s that satisfies this property.
 
- 
+-- Example 1:
 
-Example 1:
-
-Input: order = "cba", s = "abcd"
-Output: "cbad"
-Explanation: 
-"a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a". 
-Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
-Example 2:
+ Input: order = "cba", s = "abcd"
+ Output: "cbad"
+ Explanation:  
+ "a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a". 
+ Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
+-- Example 2:
 
 Input: order = "cbafg", s = "abcd"
 Output: "cbad"
@@ -42,7 +41,7 @@ Output: "cbad"
             
             System.out.println(indexes);
             String res = s;
-            String nOrder = "";
+         
         	StringBuilder str = new StringBuilder();
             for (int i = 0;i<indexes.size();i++){          
                   for (Integer it : indexes.get(i)) {           	
---
+++ src/leet/median/array/Solution.java
@@ -0,0 +1,74 @@
+package leet.median.array;
+/**
+ * Given two sorted arrays nums1 and nums2 of size m and n respectively, 
+ * return the median of the two sorted arrays.
+ * The overall run time complexity should be O(log (m+n)).
+ * Example 1:
+	Input: nums1 = [1,3], nums2 = [2]
+	Output: 2.00000
+	Explanation: merged array = [1,2,3] and median is 2.
+ * @author jjanku
+ *
+ */
+/**
+ * This is merge sort
+ * @author jjanku
+ *
+ */
+public class Solution {
+	  public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		  
+		 int[] result = merge(nums1,nums2);
+		 System.out.print("Merged arrays are ");
+		 print(result);
+		  if (result.length%2!=0) {
+			  System.out.println("Ne ket branch");
+			  return result[(result.length)/2];
+		  } else {
+			  System.out.println("Res is "+ result[result.length/2 -1 ]+result[(result.length/2)] );
+			  return (result[result.length/2]-1)+result[(result.length/2)] != 0 ? 
+					  (double) ((result[result.length/2 -1])+result[(result.length/2)])/2
+					  : 0.0;
+			
+			 
+		  }
+		  
+	  }
+	  
+	  public int[] merge(int[] nums1,int[] nums2) {
+		  int n1 = nums1.length;
+		  int n2 = nums2.length;
+		  
+		  int[] mergedArray = new int[n1+n2];
+		  
+		  int i = 0 , j = 0;
+		  int k = 0;
+		  while (i<n1 && j<n2) {
+			  if (nums1[i]<nums2[j]) {
+				  mergedArray[k++] = nums1[i++];
+			  } else {
+				  mergedArray[k++] = nums2[j++];
+			  }
+		  }
+		  
+		  while (i<n1) {
+			  mergedArray[k++] = nums1[i++];
+		  }
+		  while (j<n2) {
+			  mergedArray[k++] = nums2[j++];
+		  }
+		  return mergedArray;
+	  }
+	  void print(int[] arr) {
+		  System.out.print("\n[");
+		  for (int i : arr) {
+			  System.out.print(" "+ i);
+		  } System.out.println(" ]");
+	  }
+	  public static void main(String[] args) {
+		  int[] n1 = {1,2,4};
+		  int[] n2 = {3,5,6};
+		  Solution sol = new Solution();
+		System.out.println("Median is "+sol.findMedianSortedArrays(n1,n2));
+	  }
+}
