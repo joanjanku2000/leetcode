b2d312467350055487ee768c7b74e219906953a1
jjanku <janku.joan4@gmail.com>
2021-08-06 15:10:26 +0200

    _3sum

---
+++ src/leet/_3sum/Solution.java
@@ -0,0 +1,58 @@
+package leet._3sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution {
+	
+	 public List<List<Integer>> threeSum(int[] nums) {
+	     	
+		 List<List<Integer>> result = new ArrayList<List<Integer>>();
+		 Arrays.sort(nums);
+		 for (int i = 0;i<nums.length;i++) {
+			 int value = -nums[i];
+			 
+			 int j = i + 1;
+			 int k = nums.length - 1;
+			 
+			 while (j<k) {
+				 int sum =  nums[j] + nums[k];
+			
+				 if (value == sum) {
+					 List<Integer> temporary = new ArrayList<>();
+					 temporary.add(nums[i]);
+					 temporary.add(nums[j]);
+					 temporary.add(nums[k]);
+					 if ( !contains(result,temporary)) {
+						 result.add(temporary);
+					 }
+				 }  
+				 if (sum < value) j++;
+				 else k--;
+				if (j>nums.length || k<=0) break;
+				 }
+			 }
+		    return result;
+		 }
+	       
+	    
+	    
+	    
+	    private boolean contains(List<List<Integer>> list1, List<Integer> list2) {
+	    		    		for (List<Integer> list : list1) {
+	    			Collections.sort(list);
+	    			Collections.sort(list2);
+	    			if (list.equals(list2)) return true;
+	    		}
+	    		 return false;
+	    }
+	    
+	    public static void main(String[] args) {
+	    	int[] arr = {};
+	    	System.out.println(new Solution().threeSum(arr));
+	    }
+}
---
+++ src/leet/longest/palindromic/substring/Solution.java
@@ -0,0 +1,78 @@
+package leet.longest.palindromic.substring;
+/**
+ * Given a string s, 
+ * return the longest palindromic substring in s.
+
+ 
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Note: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+Example 3:
+
+Input: s = "a"
+Output: "a"
+Example 4:
+
+Input: s = "ac"
+Output: "a"
+ 
+ * @author jjanku august 2021 - complexity ~ O(n^2)
+ *
+ */
+public class Solution {
+	
+	static char[] characters = new char[256];
+	public String longestPalindromicSubstring(String s) {
+		if (s.isEmpty() || s.isBlank()) return "";
+		String lps = s.substring(0,1);
+		StringBuilder st = new StringBuilder();
+		String max = lps;
+		for (int i =0;i<s.length();i++) {
+			if (characters[s.charAt(i)]==0) {
+				characters[s.charAt(i)]++;
+			} else {
+				
+				int j = 0; 
+				boolean keepGoing  = true;
+				for (j = 0;j<i && keepGoing;j++) {
+					System.out.println("Passing the value "+s.substring(j,i+1));
+					if (isPalindromic(s.substring(j,i+1))) {
+						System.out.println("Found the palendromic string "+s.substring(j,i+1));
+						if(max.length()<s.substring(j,i+1).length()) {
+							max = s.substring(j,i+1);
+						} else {
+							keepGoing = false;
+						}
+					}
+				}
+			}
+		}
+		return max;
+	}
+	
+	boolean isPalindromic(String s) {
+		int j = s.length()-1;
+		for (int i = 0;i<s.length() && i!=j;i++) {
+			if(s.charAt(i)!=s.charAt(j-i)) {
+				return false;
+			} else {
+				//System.out.printf(" %c = %c",strings[i],c[j-i]);
+			}
+		}
+		return true;
+		
+	}
+	
+	public static void main(String[] args) {
+		String check = "abcdzdcab";
+		System.out.println(new Solution().longestPalindromicSubstring(check));
+	}
+
+}
